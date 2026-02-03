2bf600d02ec169b78f584c3ada91c19890327bcf
Epic__wood <youssefbouhaik0@gmail.com>
2026-02-03 22:41:44 +0100

    Update cutoff date for scraping

--- scraper.py
+++ scraper.py
@@ -15,7 +15,7 @@ SESSION_STRING = os.environ.get("TG_SESSION_STRING")
 
 TARGET_GROUP = "alm_alator"
 # Scan from NOW back to Jan 1, 2026
-CUTOFF_DATE = datetime(2026, 1, 25, tzinfo=timezone.utc)
+CUTOFF_DATE = datetime(2026, 2, 2, tzinfo=timezone.utc)
 
 # Crash prevention
 if not API_ID:
